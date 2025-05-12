093a186a3ad138b19ed2a76d6e3d0b95510967e8
Kang-Woo Lee <kwlee@etri.re.kr>
2025-05-12 14:02:46 +0900

    .

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -2,7 +2,7 @@
 
 docker image rmi kwlee0220/faaast-starter
 
-cp ../build/libs/faaast-starter-1.0.0-0.9.1-all.jar faaast-starter-all.jar
+cp ../build/libs/faaast-starter-1.0.0-1.0.0-all.jar faaast-starter-all.jar
 
 docker build -t kwlee0220/faaast-starter:latest .
 docker push kwlee0220/faaast-starter:latest
